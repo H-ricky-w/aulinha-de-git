26c86dbe7dbe20258ee862cb00f7df7dd303d87b
H-ricky-w <hricky561@gmail.com>
2024-10-09 23:37:43 -0300

    primeiro commit

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,13 @@
+package com.mycompany.mavenproject1;
+
+import com.mycompany.mavenproject1.Cacetinho;
+
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        
+        Cacetinho meuCacetinho = new Cacetinho();
+        
+        meuCacetinho.HelloWorld();
+    }
+}
