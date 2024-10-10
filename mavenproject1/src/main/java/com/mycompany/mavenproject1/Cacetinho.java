d1fee8148af3ff15a574b015077de2722917679d
H-ricky-w <hricky561@gmail.com>
2024-10-09 23:53:26 -0300

    alteracao na branch secundaria

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Cacetinho.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Cacetinho.java
@@ -2,17 +2,6 @@ package com.mycompany.mavenproject1;
 
 public class Cacetinho
 {
-    int tamanho;
-    int idade;
-    String nome;
-    
-    public void Cacetinho(int tamanho, int idade, String nome)
-    {
-        this.tamanho = tamanho;
-        this.idade = idade;
-        this.nome = nome;
-    }
-    
     public void HelloWorld()
     {
         System.out.println("Hello World!");
